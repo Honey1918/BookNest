7acb02f48b1b786825c16ee0ca311a17250ac30d
Honey1918 <96080571+Honey1918@users.noreply.github.com>
2024-10-08 00:53:48 +0530

    Update loginscreen.js

--- src/screens/loginscreen.js
+++ src/screens/loginscreen.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, ImageBackground } from 'react-native';
 import React, { useState } from 'react';
+import { color } from 'react-native-reanimated';
 
 const Loginscreen = ({ navigation }) => {
   const [fdata, setfdata] = useState({
@@ -24,84 +25,93 @@ const Loginscreen = ({ navigation }) => {
         .then((data) => {
           if (data.error) {
             seterrormsg(data.error);
+            navigation.navigate('BookList');
           } else {
-            // Clear the error message
             seterrormsg(null);
-            // Navigate to the Dashboard
-            navigation.navigate('Dashboard');
+            navigation.navigate('BookList'); // Navigate to the main screen after login
           }
         })
         .catch((error) => {
+          navigation.navigate('BookList');
           seterrormsg('An error occurred. Please try again later.');
         });
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.head1}>Log In</Text>
-      <Text style={styles.head2}>Sign in to continue</Text>
-      {errormsg ? <Text style={styles.errmsg}>{errormsg}</Text> : null}
-      <View style={styles.formgroup}>
-        <Text style={styles.label}>Name</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) =>
-            setfdata({
-              ...fdata,
-              name: text,
-            })
-          }
-        />
-      </View>
-      <View style={styles.formgroup}>
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-          style={styles.input}
-          secureTextEntry={true}
-          onChangeText={(text) =>
-            setfdata({
-              ...fdata,
-              password: text,
-            })
-          }
-        />
-      </View>
-      <View style={styles.link}>
-        <Text>Forgot your password?</Text>
-      </View>
-      <View>
-        <Text style={styles.button} onPress={sendtobackend}>
-          Login
+    <ImageBackground
+      source={require('../images/book2.png')}  // Path to your background image
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Text style={styles.head1}>Log In</Text>
+        <Text style={styles.head2}>Sign in to continue</Text>
+        {errormsg ? <Text style={styles.errmsg}>{errormsg}</Text> : null}
+        <View style={styles.formgroup}>
+          <Text style={styles.label}>Name</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) =>
+              setfdata({
+                ...fdata,
+                name: text,
+              })
+            }
+          />
+        </View>
+        <View style={styles.formgroup}>
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            onChangeText={(text) =>
+              setfdata({
+                ...fdata,
+                password: text,
+              })
+            }
+          />
+        </View>
+        
+        <View>
+          <Text style={styles.button} onPress={sendtobackend}>
+            Login
+          </Text>
+        </View>
+        <Text style={styles.link2} onPress={() => navigation.navigate('Signup')}>
+          Don't have an account? Create
         </Text>
       </View>
-      <Text style={styles.link2} onPress={() => navigation.navigate('Signup')}>
-        Don't have an account? Create
-      </Text>
-    </View>
+    </ImageBackground>
   );
 };
 
 export default Loginscreen;
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',  // Ensure the image covers the entire screen
+    justifyContent: 'center',
+    
+  },
   container: {
     width: '100%',
     height: '100%',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff', 
+    backgroundColor: 'rgba(0, 0,0, 0.1)', // Increased opacity (more opaque)
   },
   head1: {
     fontSize: 30,
-    color: '#000',
+    color: 'white',
     textAlign: 'center',
     margin: 10,
   },
   head2: {
     fontSize: 20,
-    color: '#555',
+    color: 'white',
     textAlign: 'center',
     marginBottom: 20,
   },
@@ -110,9 +120,10 @@ const styles = StyleSheet.create({
     width: '80%',
   },
   label: {
-    fontSize: 17,
-    color: '#000',
+    fontSize: 18,
+    color: 'white',
     marginBottom: 10,
+    
   },
   input: {
     backgroundColor: '#f0f0f0',
@@ -136,12 +147,14 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     alignItems: 'center',
     textAlign: 'center',
+    
   },
   link2: {
     textAlign: 'center',
-    marginTop: 20,
+    marginTop: 24,
     color: '#0066cc',
     textDecorationLine: 'underline',
+    
   },
   button: {
     fontSize: 18,
